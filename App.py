6408b80b84473dc871a49efb6a878faedc565930
YHPVY8 <47122703+YHPVY8@users.noreply.github.com>
2025-09-27 08:08:18 -0300

    Update App.py

--- App.py
+++ App.py
@@ -3,13 +3,14 @@ import streamlit as st
 import pandas as pd
 from supabase import create_client
 import altair as alt
+import os
 
 # ---- CONFIG ----
 st.set_page_config(page_title="Trading Dashboard", layout="wide")
 
 # Your Supabase credentials (move to Streamlit secrets later)
-SUPABASE_URL = "https://kjgaieellljetntsdytt.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqZ2FpZWVsbGxqZXRudHNkeXR0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1ODM2MjM5NCwiZXhwIjoyMDczOTM4Mzk0fQ.Miwj8itGx3bfUBSHOIjdZLtSYIETuoYakzYJrCt83kQ"
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 sb = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 TABLES = {
